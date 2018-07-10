cc45b599f6c7403a6538e864da9ff467060ff5ea
Frank Congson <fcongson@gmail.com>
2018-07-10 22:33:31 +1200

    Add markdown blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,14 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    'gatsby-transformer-remark'
   ],
 }
---
+++ src/pages/07-10-2018-first-blog-post/index.md
@@ -0,0 +1,6 @@
+---
+path: '/first-blog-post'
+title: 'First Blog Post'
+---
+
+Hello! This is our first blog post.
